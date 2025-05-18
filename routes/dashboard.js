e0d8f9f1163bdb46a2b29a185890b0d05d597b70
danieltrejos <tmdaneil@gmail.com>
2025-05-18 18:48:43 -0500

    vista de books/add corregida

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../lib/db');
 
-router.get('/', function(req, res, next) {
-  res.render('dashboard/index', { messages: {} });
+router.get('/', function (req, res, next) {
+    res.render('dashboard/index', { messages: {} });
 });
 
 module.exports = router;
